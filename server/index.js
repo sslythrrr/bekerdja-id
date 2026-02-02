dd165d63f9839b69877918f40f40e95c20dd12b5
Panji Anugrah <an.tubagusp@gmail.com>
2026-02-02 14:02:16 +0700

    add server api and model

---
+++ server/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import candidateRoutes from "./routes/candidateRoutes.js";
+
+dotenv.config();
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// database connection
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.uri);
+        console.log("MongoDB Connected!");
+    } catch (err) {
+        console.error("Connection Error:", err);
+        process.exit(1);
+    }
+};
+
+// routes
+app.use("/api/candidates", candidateRoutes);
+
+app.get("/", (req, res) => {
+    res.send("API is Running...");
+});
+
+// start
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server running on port ${PORT}`);
+});
---
+++ server/models/Candidate.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const CandidateSchema = new mongoose.Schema(
+    {
+        id: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        position: {
+            type: String,
+            required: true,
+        },
+        phone: {
+            type: String,
+            required: true,
+        },
+        status: {
+            type: String,
+            enum: ["new", "contacted", "interested", "rejected"],
+            default: "new",
+        },
+    },
+    { timestamps: true }
+);
+
+export default mongoose.model("Candidate", CandidateSchema);
